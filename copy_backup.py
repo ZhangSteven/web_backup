4a5679ab3da1dca1b22a18f269bbaa7b1932a435
cuser <cuser@clamc-websvr004.clamc.com.hk>
2019-07-10 10:46:52 +0800

    working on sendmail

--- copy_backup.py
+++ copy_backup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+#
 # coding=utf-8
 # 
 # Copy the backup files over from the production web server
@@ -7,20 +9,23 @@
 
 from utils.file import getFiles
 from web_backup.utility import get_backup_directory, get_user, \
-        get_password, get_remote_host
+        get_password, get_remote_host, get_current_path, \
+        getMailSender, getMailRecipients, getMailServer, \
+        getMailTimeout
 from web_backup.clear_backup import toFullPath
 from os.path import join
 from subprocess import call
 from datetime import timedelta, datetime
 from itertools import chain
+from functools import reduce
 import logging
 logger = logging.getLogger(__name__)
 
 
 
 def backupLast7days():
-	"""
-	[List] file names of backup files generated in the last 7 days.
+    """
+    [List] file names of backup files generated in the last 7 days.
 
 	The main web server generates backup files every Tuesday and Friday,
 	with the below naming convention:
@@ -28,17 +33,17 @@ def backupLast7days():
 	web-mm-dd-yy.tar.gz
 	db-mm-dd-yy.sql.gz
 	"""
-	dateString = lambda d: d.strftime('%m-%d-%y')
-	tuesOrThurs = lambda d: True if d.weekday() in [1, 4] else False
-	pastDate = lambda days: datetime.now() - timedelta(days=days)	
-	webFile = lambda dstring: 'web-' + dstring + '.tar.gz'
-	dbFile = lambda dstring: 'db-' + dstring + '.sql.gz'
+    dateString = lambda d: d.strftime('%m-%d-%y')
+    tuesOrThurs = lambda d: True if d.weekday() in [1, 4] else False
+    pastDate = lambda days: datetime.now() - timedelta(days=days)	
+    webFile = lambda dstring: 'web-' + dstring + '.tar.gz'
+    dbFile = lambda dstring: 'db-' + dstring + '.sql.gz'
 	
-	dateStrings = list(map(dateString
+    dateStrings = list(map(dateString
 				   	 	  , filter(tuesOrThurs
 						   	 	  , map(pastDate, range(1, 8)))))
 
-	return chain(map(webFile, dateStrings), map(dbFile, dateStrings))
+    return chain(map(webFile, dateStrings), map(dbFile, dateStrings))
 
 
 
@@ -53,21 +58,68 @@ def copyFromRemoteHost(fn):
 		'{0}@{1}:{2}'.format(get_user(), get_remote_host(), fn),
         get_backup_directory()	
 		]
-    print(command)
+    #print(command)
     return call(command)
+
+
+
+def doCopy():
+    """
+    ([List] successful files, [List] failed files)
+
+    Copy backup files from the remote machine (production web server),
+    then return the copy status as two lists.
+    """
+    result = lambda fn: (fn, copyFromRemoteHost(fn))
+    def build(acc, el):
+        success, failure = acc
+        fn, status = el
+        if (status == 0):
+            logger.info('doCopy(): copied {0}'.format(fn))
+            return (success + [fn], failure)
+        else:
+            logger.error('doCopy(): failed to copy {0}'.format(fn))
+            return (success, failure + [fn])
+
+    return reduce(build
+                 , map(result
+                      , map(toFullPath, backupLast7days()))
+                 , ([], []))
+
+
+
+def sendMail(success, failure):
+    """
+    [List] success, [List] failure => send email notification about the
+    status.
+    """
+    logger.debug('sendMail(): {0}, {1}'.format(success, failure))
+    list2string = lambda title, L: '{0}: {1} file(s)'.format(title, len(L)) \
+                                   + '\n' \
+                                   + '\n'.join(L)
+    body = lambda success, failure: list2string('success', success) \
+                                    + '\n\n' \
+                                    + list2string('failure', failure)
+
+    good = 'Web server backup copy result'
+    bad = 'Error occurred during web server backup copy process'
+    subject = lambda success, failture: bad if len(success) == 0 or \
+                                                len(failure) > 0 \
+                                            else good
+
+    sendMail(body(success, failure)
+            , subject(success, failure)
+            , getMailSender()
+            , getMailRecipients()
+            , getMailServer()
+            , getMailTimeout())
 
 
 
 if __name__ == '__main__':
     import logging.config
-    logging.config.fileConfig('logging.config',   
-                                disable_existing_loggers=False)
+    logging.config.fileConfig('logging.config'
+                             , disable_existing_loggers=False)
 	
     logger.info('Start copy over')
-    for fn in map(toFullPath, backupLast7days()):
-        if (copyFromRemoteHost(fn) == 0):
-            print('copied {0}'.format(fn))
-        else:
-            print('failed to copy {0}'.format(fn))
-
-        
+    sendMail(*doCopy())
---
+++ runtest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+#
+# coding=utf-8
+# 
+# Test how to run a python script directly from command line.
+#
+
+from web_backup.utility import get_current_path
+import logging
+logger = logging.getLogger(__name__)
+
+
+
+if __name__ == '__main__':
+    import logging.config
+    """
+    Note: if we want to the python script like:
+
+    /home/cuser/git/web_backup/runtest.py
+
+    Then it won't work because
+
+    1. Load the logging config file is relative to local directory
+    2. Path to log files (check the logging.config) is relative to 
+        local directory.
+
+    To make it work, we need to wrap it into a runtest.sh file, with
+    two lines inside:
+
+        cd /home/cuser/git/web_backup/
+        ./runtest.py
+
+    Check out /my_stuff/scripts/copy_back.sh
+    """
+
+    logging.config.fileConfig('logging.config'
+                             , disable_existing_loggers=False)
+    print('run test')
+    logger.info('Start copy over')
+
